3807cc9d99120108eca4a896c16f7fb3b3c27124
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-05-15 16:34:24 -0500

    Algunas correcciones

--- SumaMatrices.c
+++ SumaMatrices.c
@@ -4,13 +4,13 @@ int main(){
     int X[3][2], Y[3][2], Z[3][2];
     for(int i=0;i<3;i++){//filas son las i
         for(int j=0;j<2;j++){//columnas son las j
-            printf("Ingrese los valores de la matriz X");
+            printf("Ingrese los valores de la matriz X: ");
             scanf("%d ", &X[i][j]);
         }
     }
     for(int i=0;i<3;i++){
         for(int j=0;j<2;j++){
-            printf("Ingrese los valores de la matriz Y");
+            printf("Ingrese los valores de la matriz Y: ");
             scanf("%d ", &Y[i][j]);
         }
     }
@@ -19,9 +19,13 @@ int main(){
             Z[i][j]=X[i][j]+Y[i][j];
         }
     }
+    printf("\nLa matriz resultante es: ");
     for(int i=0;i<3;i++){
         for(int j=0;j<2;j++){
-            printf("%d \n",Z[i][j]);
+            printf("%d ",Z[i][j]);
+            if(j==2){
+                printf("\n");
+            }
         }
     }
 
