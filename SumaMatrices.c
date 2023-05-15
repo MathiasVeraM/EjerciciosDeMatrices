229df94a4fca5d1e13e47099f1a35fb472f029a3
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-05-15 16:29:44 -0500

    Lo que hicimos en clase con la ayuda del profesor

---
+++ SumaMatrices.c
@@ -0,0 +1,29 @@
+//Realizar un programa en el que se sumen dos matrices del mismo tamaño
+#include<stdio.h>
+int main(){
+    int X[3][2], Y[3][2], Z[3][2];
+    for(int i=0;i<3;i++){//filas son las i
+        for(int j=0;j<2;j++){//columnas son las j
+            printf("Ingrese los valores de la matriz X");
+            scanf("%d ", &X[i][j]);
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<2;j++){
+            printf("Ingrese los valores de la matriz Y");
+            scanf("%d ", &Y[i][j]);
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<2;j++){
+            Z[i][j]=X[i][j]+Y[i][j];
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<2;j++){
+            printf("%d \n",Z[i][j]);
+        }
+    }
+
+    return 0;
+}
