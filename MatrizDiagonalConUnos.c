b9f272a4b07783a6eb84538bc98ef2b77d4c1dc6
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-05-17 15:12:17 -0500

    Definir las variables para las columnas y filas y pedir al usuario esos datos

--- MatrizDiagonalConUnos.c
+++ MatrizDiagonalConUnos.c
@@ -1,8 +1,13 @@
 //Crear una matriz cuyas dimensiones sean dadas por el usuario, llenarla de ceros y luego imprimir la matriz con la diagonal principal llena de unos
 #include<stdio.h>
 int main(){
-
-
+    //Definir e inicializar las variables a usar para pedir datos al usuario
+    int filas=0, columnas=0;
+    //Pedir datos
+    printf("Por favor ingrese el numero de filas de la matriz: ");
+    scanf("%d",&filas);
+    printf("Por favor ingrese el numero de columnas de la matriz: ");
+    scanf("%d",&columnas);
 
 
     return 0;
