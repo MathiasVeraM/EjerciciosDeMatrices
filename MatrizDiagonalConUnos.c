053b1f218c8f2d400cb9bddf003af74bf9359a19
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-05-17 15:25:11 -0500

    Hasta ahì en teoria deberìa salir la matriz con ceros, no sè porque no me sale desde la segunda fila

--- MatrizDiagonalConUnos.c
+++ MatrizDiagonalConUnos.c
@@ -2,12 +2,24 @@
 #include<stdio.h>
 int main(){
     //Definir e inicializar las variables a usar para pedir datos al usuario
-    int filas=0, columnas=0;
+    int filas=0, columnas=0, i, j;
     //Pedir datos
     printf("Por favor ingrese el numero de filas de la matriz: ");
     scanf("%d",&filas);
     printf("Por favor ingrese el numero de columnas de la matriz: ");
     scanf("%d",&columnas);
+    //Le inicialice la matriz para que me sirva
+    int Matriz[2][2];
+    //Hacer el for para definir la matriz con ceros
+    for(i=0;i<=filas;i++){
+        for(j=0;j<=columnas;j++){
+            Matriz[i][j]=0;
+            printf("%d ", Matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+
 
 
     return 0;
