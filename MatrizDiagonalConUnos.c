0b93391e85f60b5c19b924da693925761e761406
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-05-17 15:05:32 -0500

    Indicaciones del ejercicio incluir la biblioteca stdio.h

---
+++ MatrizDiagonalConUnos.c
@@ -0,0 +1,9 @@
+//Crear una matriz cuyas dimensiones sean dadas por el usuario, llenarla de ceros y luego imprimir la matriz con la diagonal principal llena de unos
+#include<stdio.h>
+int main(){
+
+
+
+
+    return 0;
+}
